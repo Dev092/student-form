4b9fa9dbeb9a6ea8bcce9883dbd44da9fac23921
Dev kumar <thedevkumar9@gmail.com>
2024-02-16 20:17:52 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RtfComponent } from './rtf/rtf.component';
+import { TdfComponent } from './tdf/tdf.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RtfComponent,
+    TdfComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RtfComponent,
+    TdfComponent,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/tdf/tdf.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-tdf',
+  templateUrl: './tdf.component.html',
+  styleUrl: './tdf.component.css'
+})
+export class TdfComponent {
+
+  onsubmit(form:NgForm){
+    
+  }
+
+}
